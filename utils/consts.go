46ad42849fa42846fd6aed7ac213b788a065c0d2
adan-ea <57812567+adan-ea@users.noreply.github.com>
2024-04-26 09:17:12 +0200

    refacto: add Point

--- utils/consts.go
+++ utils/consts.go
@@ -5,3 +5,12 @@ const (
 	ScreenHeight = 600
 	GridSize     = 20
 )
+
+type Point struct {
+	X, Y int
+}
+
+var Up = Point{X: 0, Y: -1}
+var Down = Point{X: 0, Y: 1}
+var Left = Point{X: -1, Y: 0}
+var Right = Point{X: 1, Y: 0}
